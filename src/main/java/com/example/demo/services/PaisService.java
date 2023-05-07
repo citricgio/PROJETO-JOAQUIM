af0f201cfc5421ded26e7446bb54b59f790bf17a
unknown <itsgiomilker@yahoo.com>
2023-05-07 14:30:57 -0300

    Exceptions criadas

--- src/main/java/com/example/demo/services/PaisService.java
+++ src/main/java/com/example/demo/services/PaisService.java
@@ -1,11 +1,14 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.domain.Pais;
 import com.example.demo.repository.PaisRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class PaisService {
@@ -15,6 +18,11 @@ public class PaisService {
 	
 	public List<Pais> findAll(){
 		return repo.findAll();
+	}
+	
+	public Optional<Pais> findById(String id) {
+		Optional<Pais> pais = repo.findById(id);
+		return Optional.ofNullable(pais.orElseThrow(() -> new ObjectNotFoundException("País não encontrado")));
 	}
 
 }
